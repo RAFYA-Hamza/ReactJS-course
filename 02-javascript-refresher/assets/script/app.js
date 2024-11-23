9cd401bcc7970502f2597dd7740a15239cf782e8
RYham <rafyahamza99@gmail.com>
2024-11-23 13:29:26 +0100

    Import & Export with Javascript

---
+++ 02-javascript-refresher/assets/script/app.js
@@ -0,0 +1,8 @@
+// To import differet variable using import, there is a different methods.
+
+// import { apiKey, password as code } from "./util.js";
+
+// Imported like an object
+import * as util from "./util.js";
+
+console.log(util.apiKey);
---
+++ 02-javascript-refresher/assets/script/util.js
@@ -0,0 +1,7 @@
+// We can see different export in one file
+// But for the default export we can see just one per file
+
+export let apiKey = "qwöoihqwoh";
+export let password = "oinibiu";
+
+export default "qwerty";
