891ff27cce68271eb93da1c7dab9016299b2c937
RYham <rafyahamza99@gmail.com>
2024-11-23 15:15:27 +0100

    In this part, we discover differents types of variables, also we see the objects & classes concept

--- 02-javascript-refresher/assets/script/app.js
+++ 02-javascript-refresher/assets/script/app.js
@@ -1,3 +1,7 @@
+/*
+    -> Import & Export 
+*/
+
 // To import differet variable using import, there is a different methods.
 
 // import { apiKey, password as code } from "./util.js";
@@ -6,3 +10,72 @@
 import * as util from "./util.js";
 
 console.log(util.apiKey);
+/*---------------------------------------------------------------------------*/
+
+/*
+    -> Revisiting Variables, Values & operators
+*/
+
+// -> In javascript not allowed to name the variable using lowercase, -, some specific caracters
+// -> The best practice it's to use camelCase
+
+const userMessage = "Hello world!";
+let userName = "Hamza RAFYA";
+
+console.log(userMessage);
+console.log(userName);
+
+console.log(10 === 10);
+console.log(10 * 10);
+
+function greetUser(userName, userMessage) {
+  console.log("User Name:", userName);
+  console.log("Message:", userMessage);
+
+  return "Hi! I'm " + userName + ". " + userMessage;
+}
+
+console.log(greetUser(userName, userMessage));
+
+// -> We can also declare an Arrow function
+
+export default (userName, userMessage) => {
+  console.log("Hello!");
+
+  return userName + userMessage;
+};
+
+// -> We can combine differents types of variables in one variable using the objects
+
+const user = {
+  name: "Hamza",
+  age: 25,
+  greet() {
+    console.log("Hello!");
+    console.log(this.name);
+  },
+};
+
+console.log(user.age);
+user.greet();
+
+class User {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  greet() {
+    console.log("Hello! under class");
+  }
+}
+
+const user1 = new User("Hamza RAFYA", 25);
+
+console.log(user1);
+
+/*---------------------------------------------------------------------------*/
+
+/*
+    -> Import & Export 
+*/
