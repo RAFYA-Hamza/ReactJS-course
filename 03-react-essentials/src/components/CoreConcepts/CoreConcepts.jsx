ae8ed15843f2b0519f880c50e60be5c4da87d887
RYham <rafyahamza99@gmail.com>
2024-11-26 00:08:06 +0100

    Forwarding Props

--- 03-react-essentials/src/components/CoreConcepts/CoreConcepts.jsx
+++ 03-react-essentials/src/components/CoreConcepts/CoreConcepts.jsx
@@ -1,11 +1,24 @@
-import "./CoreConcepts.css";
+import CoreConcept from "./CoreConcept.jsx";
+import { CORE_CONCEPTS } from "../../data.js";
+import Section from "../Section.jsx";
 
-export default function CoreConcept({ image, title, description }) {
+export default function CoreConcepts() {
   return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
+    <Section title={"Core Concepts"} id="core-concepts">
+      <h2>Core Concepts</h2>
+      <ul>
+        {CORE_CONCEPTS.map((conceptItem, index) => (
+          <CoreConcept key={index} {...conceptItem} />
+        ))}
+        {/* <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} /> */}
+      </ul>
+    </Section>
   );
 }
