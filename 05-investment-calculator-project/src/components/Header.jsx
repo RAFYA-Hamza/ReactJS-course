a28f91e5400e4ccf37f40a4d113e030face852b0
RYham <rafyahamza99@gmail.com>
2024-12-02 19:46:07 +0100

    Adding a Header Component

--- 05-investment-calculator-project/src/components/Header.jsx
+++ 05-investment-calculator-project/src/components/Header.jsx
@@ -1,3 +1,10 @@
+import headerImg from "../assets/investment-calculator-logo.png";
+
 export default function Header() {
-  return <header></header>;
+  return (
+    <header id="header">
+      <img src={headerImg} alt="Header image" />
+      <h1>React Investment Calculator</h1>
+    </header>
+  );
 }
