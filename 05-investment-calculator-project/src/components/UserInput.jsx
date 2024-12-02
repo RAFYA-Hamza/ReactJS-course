c3343df111e8668d45376f7418f2663c91aa2b2a
RYham <rafyahamza99@gmail.com>
2024-12-02 21:52:23 +0100

    Lifting State Up

--- 05-investment-calculator-project/src/components/UserInput.jsx
+++ 05-investment-calculator-project/src/components/UserInput.jsx
@@ -1,33 +1,52 @@
-import { useState } from "react";
-
-export default function UserInput() {
-  const [userInput, setUserInput] = useState({
-    initialInvestment: 1000,
-    annualInvestment: 1200,
-    expectedReturn: 6,
-    duration: 10,
-  });
-
+export default function UserInput({ onChangeInput, userInput }) {
   return (
     <section id="user-input">
       <div className="input-group">
         <p>
           <label htmlFor="">Initial Investment</label>
-          <input type="number" required />
+          <input
+            value={userInput.initialInvestment}
+            onChange={(event) => {
+              onChangeInput("initialInvestment", event.target.value);
+            }}
+            type="number"
+            required
+          />
         </p>
         <p>
           <label htmlFor="">Annual Investment</label>
-          <input type="number" required />
+          <input
+            value={userInput.annualInvestment}
+            onChange={(event) => {
+              onChangeInput("annualInvestment", event.target.value);
+            }}
+            type="number"
+            required
+          />
         </p>
       </div>
       <div className="input-group">
         <p>
           <label htmlFor="">Expected Return</label>
-          <input type="number" required />
+          <input
+            value={userInput.annualInvestment}
+            onChange={(event) => {
+              onChangeInput("expectedReturn", event.target.value);
+            }}
+            type="number"
+            required
+          />
         </p>
         <p>
           <label htmlFor="">Duration</label>
-          <input type="number" required />
+          <input
+            value={userInput.annualInvestment}
+            onChange={(event) => {
+              onChangeInput("duration", event.target.value);
+            }}
+            type="number"
+            required
+          />
         </p>
       </div>
     </section>
