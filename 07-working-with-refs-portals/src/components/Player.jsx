4ba0ba3e0bc6d2a98aac2237a70f0a58846310be
RYham <rafyahamza99@gmail.com>
2024-12-07 09:14:24 +0100

    Manipulate the DOM via Refs

--- 07-working-with-refs-portals/src/components/Player.jsx
+++ 07-working-with-refs-portals/src/components/Player.jsx
@@ -1,10 +1,20 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  const inputName = useRef();
+  const [enteredName, setEnteredName] = useState(null);
+
+  function handleClick() {
+    setEnteredName(inputName.current.value);
+    inputName.current.value = "";
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {enteredName ?? "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={inputName} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
