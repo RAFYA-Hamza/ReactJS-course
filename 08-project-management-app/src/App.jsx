eee5dcc76fbcd04c2850f00cebae82bd9a8912b7
RYham <rafyahamza99@gmail.com>
2024-12-08 16:15:58 +0100

    Splitting Components to Split JSX & Tailwind Styles (for Higher Reusability

--- 08-project-management-app/src/App.jsx
+++ 08-project-management-app/src/App.jsx
@@ -1,11 +1,19 @@
 import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 import SideBar from "./components/SideBar";
+import { useState } from "react";
 
 function App() {
+  const [newProject, setNewProject] = useState(false);
+
+  function handleClick() {
+    setNewProject(true);
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <SideBar />
-      <NewProject />
+      <SideBar onSelect={handleClick} />
+      {newProject ? <NewProject /> : <NoProjectSelected />}
     </main>
   );
 }
