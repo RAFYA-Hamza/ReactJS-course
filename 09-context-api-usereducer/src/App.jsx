245741769ce394cc6f7dcf321a5184a28855ba7e
RYham <rafyahamza99@gmail.com>
2024-12-12 23:00:04 +0100

    Creating & Providing The Context

--- 09-context-api-usereducer/src/App.jsx
+++ 09-context-api-usereducer/src/App.jsx
@@ -5,6 +5,7 @@ import Shop from "./components/Shop.jsx";
 import Product from "./components/Product.jsx";
 
 import { DUMMY_PRODUCTS } from "./dummy-products.js";
+import { CartContext } from "./store/shopping-cart-context.jsx";
 
 function App() {
   const [shoppingCart, setShoppingCart] = useState({
