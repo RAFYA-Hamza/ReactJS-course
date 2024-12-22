06f4c24ad3b83985bd0775f9a857c0048679f046
RYham <rafyahamza99@gmail.com>
2024-12-22 18:33:01 +0100

    Fetch Data using async and await

--- 14-connecting-react-project-to-backend/src/components/AvailablePlaces.jsx
+++ 14-connecting-react-project-to-backend/src/components/AvailablePlaces.jsx
@@ -5,13 +5,23 @@ export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:3000/places")
-      .then((response) => {
-        return response.json();
-      })
-      .then((resData) => {
-        setAvailablePlaces(resData.places);
-      });
+    async function fetchData() {
+      const response = fetch("http://localhost:3000/places");
+      const resData = response.json();
+
+      setAvailablePlaces(resData.places);
+    }
+
+    fetchData();
+
+    // First method without using async function
+    // fetch("http://localhost:3000/places")
+    //   .then((response) => {
+    //     return response.json();
+    //   })
+    //   .then((resData) => {
+    //     setAvailablePlaces(resData.places);
+    //   });
   }, []);
 
   return (
