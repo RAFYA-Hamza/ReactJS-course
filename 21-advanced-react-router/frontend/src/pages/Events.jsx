e8491978e27f1cb3396ed9cfe49458d77fd309c5
RYham <rafyahamza99@gmail.com>
2025-01-01 22:44:29 +0100

    How to use the loader data in the App component

--- 21-advanced-react-router/frontend/src/pages/Events.jsx
+++ 21-advanced-react-router/frontend/src/pages/Events.jsx
@@ -1,24 +1,21 @@
-import { Link } from "react-router-dom";
-
-const DUMMY_EVENTS = [
-  { id: "e1", title: "Event 1" },
-  { id: "e2", title: "Event 2" },
-  { id: "e3", title: "Event 3" },
-  { id: "e4", title: "Event 4" },
-  { id: "e5", title: "Event 5" },
-  { id: "e6", title: "Event 6" },
-];
+import { useLoaderData } from "react-router-dom";
+import EventsList from "../components/EventsList";
 
 function EventsPage() {
-  return (
-    <ul>
-      {DUMMY_EVENTS.map((event) => (
-        <li key={event.id}>
-          <Link to={`/events/${event.id}`}>{event.title}</Link>
-        </li>
-      ))}
-    </ul>
-  );
+  const events = useLoaderData();
+
+  return <EventsList events={events} />;
 }
 
 export default EventsPage;
+
+export const loader = async () => {
+  const response = await fetch("http://localhost:8080/events");
+
+  if (!response.ok) {
+    // handle errors
+  } else {
+    const resData = await response.json();
+    return resData.events;
+  }
+};
